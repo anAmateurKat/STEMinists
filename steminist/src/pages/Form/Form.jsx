8a2650f5736ffdaf118988e8ae76ed8fc2116755
cpostma3 <cpostma3@uwo.ca>
2022-01-08 15:21:31 -0500

    minor form adjustments

--- steminist/src/pages/Form/Form.jsx
+++ steminist/src/pages/Form/Form.jsx
@@ -8,39 +8,39 @@ export const Form = () => {
          <div className='row'>
              <h2>Please fill this form to update your profile</h2>
              <div className='firstname'>
-                 <label>First Name</label>
+                 <label>First Name: </label>
                  <input type={"text"} placeholder='First Name' />
              </div>
              <div className='lastname'>
-                 <label>Last Name</label>
+                 <label>Last Name: </label>
                  <input type={"text"} placeholder='Last Name' />
              </div>
              <div className='email'>
-                 <label>Email</label>
+                 <label>Email: </label>
                  <input type={"text"} placeholder='Email' />
              </div>
              <div className='location'>
-                 <label>Location</label>
+                 <label>Location: </label>
                  <input type={"text"} placeholder='Location' />
              </div>
              <div className='pronouns'>
-                 <label>Pronouns</label>
+                 <label>Pronouns: </label>
                  <input type={"text"} placeholder='Pronouns' />
              </div>
              <div className='tech-interest'>
-                 <label>Tech Interests</label>
+                 <label>Tech Interests: </label>
                  <input type={"text"} placeholder='Tech Interests' />
              </div>
              <div className='Area-of-discussion'>
-                 <label>Areas of discussion</label>
+                 <label>Areas of Discussion: </label>
                  <input type={"text"} placeholder='Areas of Discussion' />
              </div>
              <div className='linkedin'>
-                 <label>Linkedin Url</label>
+                 <label>Linkedin Url: </label>
                  <input type={"text"} placeholder='Linkedin profile Url' />
              </div>
              <div className='intro'>
-                 <label>Introduction/Summary</label>
+                 <label>Introduction/Summary: </label>
                  <input type={"text"} placeholder='Introduction' />
              </div>
          </div>
